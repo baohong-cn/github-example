764b0ad7b25565b283b948171dcb7c56612e635c
BaoHong <baohong@BaoHongdeMacBook-Pro.local>
2020-02-11 15:49:58 +0800

    the notebook is for Capstone project

---
+++ Capstone_Project_Notebook.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+### This notebook will be mainly used for the capstone project.
+
+import pandas as pd # library for data analsysis
+import numpy as np # library to handle data in a vectorized mannerImport Numpy as np
+
+###Print the following the statement: Hello Capstone Project Course!
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
